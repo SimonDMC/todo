1621ad74f2951cefc282c1758c252353e976f299
SimonDMCPlayer <46278840+SimonDMCPlayer@users.noreply.github.com>
2022-08-18 09:48:05 +0200

    Add control bar

--- src/components/TodoBoard.tsx
+++ src/components/TodoBoard.tsx
@@ -13,7 +13,7 @@ const TodoBoard = (props: TodoBoardProps) => {
     const style: React.CSSProperties = {
         width: '35em',
         height: '100%',
-        border: '7px solid #fff',
+        border: '0.4em solid #fff',
         borderRadius: '3em',
         backgroundColor: '#FDFFF2',
         listStyle: 'none',
@@ -21,8 +21,6 @@ const TodoBoard = (props: TodoBoardProps) => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        overflowY: 'auto',
-        scrollBehavior: 'smooth',
         overflowX: 'hidden',
         position: 'relative',
     }
@@ -34,6 +32,30 @@ const TodoBoard = (props: TodoBoardProps) => {
         backgroundColor: '#00000000',
         marginTop: '2.5%',
         color: '#00000080',
+        width: '100%',
+    }
+
+    const wrapperStyle: React.CSSProperties = {
+        marginBottom: '3em',
+        borderBottom: '0.02em solid #81818180',
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        overflowY: 'auto',
+        scrollBehavior: 'smooth',
+        backgroundClip: 'padding-box',
+    }
+
+    const bottomBarStyle: React.CSSProperties = {
+        width: '100%',
+        height: 'calc((3em - 0.4em) / 1.5)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        position: 'absolute',
+        bottom: '0',
+        fontSize: '1.5em',
+        color: '#777',
     }
 
     const setTodos = (todos: TodoItemType[]) => {
@@ -41,9 +63,10 @@ const TodoBoard = (props: TodoBoardProps) => {
     }
 
     return (
-            <ul
-            className="todo-list"
-            style={style}>
+        <ul
+        className="todo-list"
+        style={style}>
+            <div className='list-wrapper' style={wrapperStyle}>
                 <input 
                     type='text' 
                     placeholder='Board Name'
@@ -61,7 +84,14 @@ const TodoBoard = (props: TodoBoardProps) => {
                         />
                     );
                 })}
-            </ul>
+            </div>
+            <div className='bottom-bar' style={bottomBarStyle}>
+                <i className="fa-solid fa-arrow-left"></i>
+                <i className="fa-solid fa-arrow-right"></i>
+                <i className="fa-solid fa-plus"></i>
+                <i className="fa-solid fa-minus"></i>
+            </div>
+        </ul>
     );
 }
 
